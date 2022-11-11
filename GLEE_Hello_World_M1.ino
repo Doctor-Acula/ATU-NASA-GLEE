4efba979262682362a1898e90ae7a56ffab9cc62
Doctor-Acula <109109991+Doctor-Acula@users.noreply.github.com>
2022-11-11 15:17:41 -0600

    Add files via upload

---
+++ GLEE_Hello_World_M1.ino
@@ -0,0 +1,12 @@
+void setup() 
+{
+  // put your setup code here, to run once:
+Serial.begin(9600); //sets the baud rate, which means data is transmitted at 9600 bits/sec
+Serial.println("Hello World!"); //No true programmer goes without a "Hello World!"
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+
+}
