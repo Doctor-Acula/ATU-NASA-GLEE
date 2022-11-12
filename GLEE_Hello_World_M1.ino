19259c5b8c53dd37319cc7677cb4948dbff9cb6c
Doctor-Acula <109109991+Doctor-Acula@users.noreply.github.com>
2022-11-11 18:15:53 -0600

    Update GLEE_Hello_World_M1.ino

--- GLEE_Hello_World_M1.ino
+++ GLEE_Hello_World_M1.ino
@@ -1,12 +1,13 @@
 void setup() 
 {
   // put your setup code here, to run once:
-Serial.begin(9600); //sets the baud rate, which means data is transmitted at 9600 bits/sec
-Serial.println("Hello World!"); //No true programmer goes without a "Hello World!"
+Serial.begin(9600); //Sets the information transmission rate in bits/second
+//Serial.println("Hello World!"); //No true programmer goes without a "Hello World!"
 }
 
 void loop() 
 {
   // put your main code here, to run repeatedly:
-
+Serial.println("Hello World!"); //Should print multiple times, as it's in a loop
+delay(5000); //Delays the loop by 5000 milliseconds (5 seconds)
 }
